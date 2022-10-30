83af354fea8d64053276680f0350d30a807a7791
ygf11 <yanggf23@gmail.com>
2022-10-30 07:23:11 -0400

    feat: add github action

--- fuzzing-tests/parquet2csv.py
+++ fuzzing-tests/parquet2csv.py
@@ -11,6 +11,7 @@ for file in os.listdir("testdata"):
     if file.startswith("test"):
         file_name = Path(file).stem
         print("{} {} {}".format("testdata/" + file, file_name, "testdata/"+file_name+".csv"))
-        df = pd.read_parquet('testdata/{}.parquet'.format(file_name))
+        df = pd.read_parquet('testdata/{}.parquet'.format(file_name), use_nullable_dtypes = True)
+        print(df.dtypes)
         df.to_csv('testdata/{}.csv'.format(file_name), index = False)
 
